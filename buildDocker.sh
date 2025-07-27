e069e8faa1bbcb59e93a57da518b2d0aca6a5298
Eerriicckk <erickrumpel67@gmail.com>
2025-07-27 16:50:39 -0300

    refactor: container inicia no reset do server

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,4 +2,4 @@ sudo docker stop container_card_creator
 sudo docker rm container_card_creator
 sudo docker rmi card_creator
 sudo docker build -t card_creator .
-sudo docker run -d --name container_card_creator -p 5174:5174 card_creator
+sudo docker run -d --restart=always --name container_card_creator -p 5174:5174 card_creator
